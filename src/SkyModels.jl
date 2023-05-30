06d98d022812d3eb306c80145a1191e82622a3d2
Paul Tiede <ptiede91@gmail.com>
2023-05-30 14:17:52 -0400

    add reexport

--- src/SkyModels.jl
+++ src/SkyModels.jl
@@ -13,11 +13,39 @@ using FFTW
 using NFFT
 using NamedTupleTools
 using PaddedViews
+using Reexport
 using RecipesBase
 using StaticArrays
 using StructArrays
 using LinearAlgebra
 using Printf
+
+@reexport using ComradeBase
+
+"""
+    rad2μas(x)
+Converts a number from radians to micro-arcseconds (μas)
+"""
+@inline rad2μas(x) = 180*3600*1_000_000*x/π
+
+"""
+    μas2rad(x)
+Converts a number from micro-arcseconds (μas) to rad
+"""
+@inline μas2rad(x) = x/(180*3600*1_000_000)*π
+
+export linearpol, mbreve, evpa, rad2μas, μas2rad, create_cache
+
+using ComradeBase: AbstractDims, AbstractModel, AbstractPolarizedModel
+
+import ComradeBase: flux, radialextent, intensitymap, intensitymap!,
+                    intensitymap_analytic, intensitymap_analytic!,
+                    intensitymap_numeric, intensitymap_numeric!,
+                    visibilities, visibilities!,
+                    _visibilities, _visibilities!,
+                    visibilities_analytic, visibilities_analytic!,
+                    visibilities_numeric, visibilities_numeric!
+
 
 # Write your package code here.
 include(joinpath(@__DIR__, "models/models.jl"))
