d7c35b4d850d003a484f53af1290060fb2879dfa
Paul Tiede <ptiede91@gmail.com>
2023-07-23 18:38:04 -0400

    work on 1.8

--- src/models/clean.jl
+++ src/models/clean.jl
@@ -16,7 +16,7 @@ struct MultiComponentModel{M, F, V<:AbstractVector} <: AbstractModel
 end
 
 flux(m::MultiComponentModel)  = flux(m.base)*sum(m.flux)
-radialextent(m::MultiComponentModel) = 2*maximum(splat(hypot), zip(m.x, m.y)) + radialextent(m.base)
+radialextent(m::MultiComponentModel) = 2*maximum(x->hypot(x...), zip(m.x, m.y)) + radialextent(m.base)
 
 Base.getindex(m::MultiComponentModel, i::Int) = modify(m.base, Shift(m.x[i], m.y[i]), Renormalize(m.flux[i]))
 
