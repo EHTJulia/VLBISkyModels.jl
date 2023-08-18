35c7eeebc85610f8457798ae5fbefcaac10c4de4
Paul Tiede <ptiede91@gmail.com>
2023-08-18 08:00:21 -0400

    add regrid method

--- src/utility.jl
+++ src/utility.jl
@@ -8,11 +8,11 @@ function centroid_mean(imgs::AbstractVector{<:IntensityMap})
 end
 
 """
-    center_image(img::IntensityMap)
+    center_image(img::SpatialIntensityMap)
 
 centers the `img` such that the centroid of the image is approximately at the origin.
 """
-function center_image(img::IntensityMap)
+function center_image(img::SpatialIntensityMap)
     x, y = centroid(img)
     return modify(img, Shift(-x, -y))
 end
@@ -22,7 +22,7 @@ end
 struct InterpolatedImage{I,P} <: AbstractModel
     img::I
     itp::P
-    function InterpolatedImage(img::IntensityMap)
+    function InterpolatedImage(img::SpatialIntensityMap)
         itp = BicubicInterpolator(img.X, img.Y, img, StrictBoundaries())
         return new{typeof(img), typeof(itp)}(img, itp)
     end
@@ -37,7 +37,7 @@ function intensity_point(m::InterpolatedImage, p)
     (m.img.Y[begin] > p.Y || p.Y > m.img.Y[end]) && return zero(eltype(m.img))
     return m.itp(p.X, p.Y)/(step(m.img.X)*step(m.img.Y))
 end
-function ModifiedModel(img::IntensityMap, transforms)
+function ModifiedModel(img::SpatialIntensityMap, transforms)
     ms = ModifiedModel(InterpolatedImage(img), transforms)
     return intensitymap(ms, axiskeys(img))
 end
@@ -67,7 +67,7 @@ convolve!(img, Gaussian())
 This method does not automatically pad your image. If there is substantial flux at the boundaries
 you will start to see artifacts.
 """
-function convolve!(img::IntensityMap{<:Real}, m::AbstractModel)
+function convolve!(img::SpatialIntensityMap{<:Real}, m::AbstractModel)
     @assert visanalytic(typeof(m)) isa IsAnalytic "Convolving model must have an analytic Fourier transform currently"
     p = plan_rfft(baseimage(img))
 
@@ -101,15 +101,40 @@ For the inplace version of the function see [`convolve!`](@ref)
 This method does not automatically pad your image. If there is substantial flux at the boundaries
 you will start to see artifacts.
 """
-function convolve(img::IntensityMap, m::AbstractModel)
+function convolve(img::SpatialIntensityMap, m::AbstractModel)
     cimg = copy(img)
     return convolve!(cimg, m)
 end
 
-function convolve!(img::IntensityMap{<:StokesParams}, m)
+function convolve!(img::SpatialIntensityMap{<:StokesParams}, m)
     convolve!(stokes(img, :I), m)
     convolve!(stokes(img, :Q), m)
     convolve!(stokes(img, :U), m)
     convolve!(stokes(img, :V), m)
     return img
+end
+
+
+
+
+
+"""
+    regrid(img::IntensityMap, g::GriddedKeys)
+
+Regrids the spatial parts of an image `img` on the new domain `g`
+"""
+function regrid(img::IntensityMap, g::GriddedKeys)
+    map(eachslice(img; dims=(:X, :Y))) do simg
+        return regrid(simg, g)
+    end
+end
+
+function regrid(img::IntensityMap, nx::Int, ny::Int, fovx::Real, fovy::Real, x0=0.0, y0=0.0)
+    g = imagepixels(nx, ny, fovx, fovy, x0, y0)
+    return regrid(img, g)
+end
+
+function regrid(img::SpatialIntensityMap, g::GriddedKeys)
+    fimg = VLBISkyModels.InterpolatedImage(img)
+    return intensitymap(fimg, g)
 end
