e12f33b8f5030b7609347ae589e9be9de3196d4c
Paul Tiede <ptiede91@gmail.com>
2023-10-05 12:30:19 -0400

    fix viz test

--- test/viz.jl
+++ test/viz.jl
@@ -1,5 +1,5 @@
 @testset "Makie Visualizations" begin
-    m = PolarizedModel(Gaussian(), 0.1*Gaussian(), 0.25*Gausian(), 0.1*Gaussian())
+    m = PolarizedModel(Gaussian(), 0.1*Gaussian(), 0.25*Gaussian(), 0.1*Gaussian())
     g = imagepixels(10.0, 10.0, 256, 256)
     img = intensitymap(m, g)
 
