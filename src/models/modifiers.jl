29944532bfe9a20ae45022de25db23a129092756
Paul Tiede <ptiede91@gmail.com>
2024-12-27 14:03:51 -0500

    Update src/models/modifiers.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/models/modifiers.jl
+++ src/models/modifiers.jl
@@ -440,7 +440,7 @@ true
 renormed(model::M, f) where {M<:AbstractModel} = ModifiedModel(model, Renormalize(f))
 @inline doesnot_uv_modify(::Renormalize) = true
 
-const ModNum = Union{Number, ComradeBase.DomainParams}
+const ModNum = Union{Number,ComradeBase.DomainParams}
 
 Base.:*(model::AbstractModel, f::ModNum) = renormed(model, f)
 Base.:*(f::ModNum, model::AbstractModel) = renormed(model, f)
