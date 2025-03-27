8943b363241b9795b16ae3c6efb4301cb8907318
Paul Tiede <ptiede91@gmail.com>
2025-03-27 00:04:01 -0400

    Update src/io.jl

--- src/io.jl
+++ src/io.jl
@@ -5,8 +5,7 @@ export load_fits, save_fits
 
 This loads in a fits file that is more robust to the various imaging algorithms
 in the EHT, i.e. is works with clean, smili, eht-imaging.
-The function returns an tuple with an intensitymap of type T and a second named tuple with ancillary
-information about the image, like the source name, location, mjd, and radio frequency.
+The function returns an `T<:IntensityMap`. By default if `T===IntensityMap` then we only return the Stokes I image. If `T<:IntensityMap{StokesParams}` then we return the full Stokes image.  
 """
 function load_fits(file, T::Type{<:IntensityMap})
     if !endswith(file, ".fits")
